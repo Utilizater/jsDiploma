d6845a23d43a75e9cec483249041e16e7f6acb5f
Utilizater <polevoynikita@yandex.ru>
2019-03-28 21:52:56 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ function slider(){
   let slideIndex = 1,
       slides = document.querySelectorAll(".main-slider-item");
   showSlides(slideIndex);
+  document.getElementsByClassName("container")[1].style.overflow = "hidden";
+//  console.log(document.getElementsByClassName("container")[0].style.hight);
 
   function showSlides(n) {
     if (n > slides.length) {
@@ -13,16 +15,18 @@ function slider(){
       slideIndex = slides.length;
       //n = slides.length;
     }
+    
     slides.forEach((item) => item.style.display = "none");
     slides[slideIndex - 1].style.display = "block";
-    document.getElementsByClassName("main-slider")[0].style.overflow = "hidden";
+   // document.getElementsByClassName("main-slider")[0].style.overflow = "hidden";
  
     let hight = slides[slideIndex - 1].clientHeight;
+    console.log(hight);
     slides[slideIndex - 1].style.marginTop = "-" + hight +  "px";
     let sliderAnimation = () => {
       let mt = +slides[slideIndex - 1].style.marginTop.replace('px', ""); 
-      if(mt < -5) 
-      slides[slideIndex - 1].style.marginTop = mt + 20 + "px"; 
+      if(mt < -15) 
+      slides[slideIndex - 1].style.marginTop = mt + hight/34 + "px"; 
       else clearInterval(z);
     } 
     let z = setInterval(sliderAnimation, 20);
