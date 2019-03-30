b372ad61f82cd453fa7f7f6319b20f1213216c9c
Utilizater <polevoynikita@yandex.ru>
2019-03-30 13:39:02 +0100

    calc

--- src/index.js
+++ src/index.js
@@ -143,7 +143,6 @@ let reqPopupGift = document.getElementsByClassName("reqPopupGift")[0],
     }
 
     ///add styles 
-    //styles-2
     let addStyles = document.getElementsByClassName("button-styles")[0],
         hiddenStyles = document.getElementsByClassName("styles-2");
     addStyles.addEventListener('click', () => {
@@ -155,3 +154,69 @@ let reqPopupGift = document.getElementsByClassName("reqPopupGift")[0],
         addStyles.style.display = "none";
       }
     });
+
+    //calculator
+
+    let pictureSize = document.getElementById("size"),
+        pictureMaterial = document.getElementById("material"),
+        options = document.getElementById("options"),
+        promocod = document.getElementsByClassName("promocode")[0],
+        calcPrice = document.getElementsByClassName("calc-price")[0];
+
+    mapping = {
+      "40x50": 500,
+      "50x70": 1000,
+      "70x70": 1500,
+      "70x100": 2000,
+      "Холст из волокна": 1000,
+      "Льняной холст": 1500,
+      "Холст из натурального хлопка": 2000,
+      "Покрытие арт-гелем": 1000,
+      "Экспресс-изготовление": 1500,
+      "Доставка готовых работ": 2000 
+    };
+
+    function toCount(promo) {
+      let a;
+      if (options.value == "Дополнительные услуги") a = 0;
+      else a = mapping[options.value];
+      if (promo) 
+      return 0.7 * (mapping[pictureMaterial.value] + mapping[pictureMaterial.value] + a) + " рублей";
+      else return mapping[pictureMaterial.value] + mapping[pictureMaterial.value] + a + " рублей";
+    }; 
+
+    pictureSize.addEventListener('change', () => {
+      if(pictureSize.value != "Выберите размер картины" && pictureMaterial.value != "Выберите материал картины") {
+        if (promocod.value == "IWANTPOPART")
+            calcPrice.textContent = toCount(true);
+            else calcPrice.textContent = toCount(false);
+      } else  calcPrice.textContent = "Для расчета нужно выбрать размер картины и материал картины";
+    });
+
+    pictureMaterial.addEventListener('change', () => {
+      if(pictureSize.value != "Выберите размер картины" && pictureMaterial.value != "Выберите материал картины") {
+        if (promocod.value == "IWANTPOPART")
+            calcPrice.textContent = toCount(true);
+            else calcPrice.textContent = toCount(false);
+      } else  calcPrice.textContent = "Для расчета нужно выбрать размер картины и материал картины";
+    });
+
+    options.addEventListener('change', () => {
+      if(pictureSize.value != "Выберите размер картины" && pictureMaterial.value != "Выберите материал картины") {
+        if (promocod.value == "IWANTPOPART")
+            calcPrice.textContent = toCount(true);
+            else calcPrice.textContent = toCount(false);
+      } else  calcPrice.textContent = "Для расчета нужно выбрать размер картины и материал картины";
+    });
+
+    promocod.addEventListener('change', () => {
+      if(pictureSize.value != "Выберите размер картины" && pictureMaterial.value != "Выберите материал картины") {
+        if (promocod.value == "IWANTPOPART")
+            calcPrice.textContent = toCount(true);
+            else calcPrice.textContent = toCount(false);
+      } else  calcPrice.textContent = "Для расчета нужно выбрать размер картины и материал картины";
+    });
+
+    /* setInterval(function() {
+      console.log(toCount(false));
+    }, 1000);*/
