f427f8c9e36834bc93e2c323da6feb8f7cad69f6
Utilizater <polevoynikita@yandex.ru>
2019-03-31 14:40:17 +0200

    accordeon

--- src/index.js
+++ src/index.js
@@ -381,20 +381,90 @@ function secondSlider() {
   function animate(element) {
     element.style.display = "block";
     element.style.width = "900px";
-   // element.style.border = "2px solid";
+    // element.style.border = "2px solid";
     element.style.marginLeft = "-1100px";
     let sliderAnimation = () => {
       let mt = +element.style.marginLeft.replace('px', "");
       if (mt < -15)
-      element.style.marginLeft = mt + 600 / 34 + "px";
+        element.style.marginLeft = mt + 600 / 34 + "px";
       else clearInterval(z);
     }
     let z = setInterval(sliderAnimation, 20);
   }
 
-  setInterval(function(){plusSlides(1)}, 5000);
+  setInterval(function () {
+    plusSlides(1)
+  }, 5000);
 
 }
 
 secondSlider();
 
+//accordion
+function accordion() {
+  let accordionHeading = document.getElementsByClassName("accordion-heading"),
+      accordionBlock = document.getElementsByClassName("accordion-block");
+
+  function hideAll() {
+    for (let i = 0; i < accordionBlock.length; i++) {
+      accordionBlock[i].style.display = "none";
+      accordionHeading[i].classList.remove("accHeadline");
+    }
+  }
+  hideAll();
+
+//accHeadline
+
+  for (let i = 0;  i < accordionHeading.length; i++) {
+    accordionHeading[i].addEventListener('click', function() {
+    //  hideAll();
+      if(accordionHeading[i].id == "accHead-1") {
+        if (document.getElementById("accBody-1").style.display == "none") {
+          hideAll();
+          accAnimate(document.getElementById("accBody-1"));
+//          document.getElementById("accBody-1").style.display = "block";
+          document.getElementById("accHead-1").classList.add("accHeadline");
+        } else hideAll();
+      } 
+
+      if(accordionHeading[i].id == "accHead-2") {
+        if (document.getElementById("accBody-2").style.display == "none") {
+          hideAll();
+          accAnimate(document.getElementById("accBody-2"));;
+          document.getElementById("accHead-2").classList.add("accHeadline");
+        } else hideAll();
+      }
+
+
+      if(accordionHeading[i].id == "accHead-3") {
+        if (document.getElementById("accBody-3").style.display == "none") {
+          hideAll();
+          accAnimate(document.getElementById("accBody-3"));
+          document.getElementById("accHead-3").classList.add("accHeadline");
+        } else hideAll();
+      }
+
+
+      if(accordionHeading[i].id == "accHead-4") {
+        if (document.getElementById("accBody-4").style.display == "none") {
+          hideAll();
+          accAnimate(document.getElementById("accBody-4"));
+          document.getElementById("accHead-4").classList.add("accHeadline");
+        } else hideAll();
+      }
+    });
+  }
+  function accAnimate(element) {
+    element.style.opacity = "0.1";
+    element.style.display = 'block';
+
+       let Anim = () => {
+         if (+element.style.opacity != 1)
+         element.style.opacity = +element.style.opacity + 0.1 + "";
+         else clearInterval(z);
+       };
+       let z = setInterval(Anim, 50);
+  }
+};
+
+accordion();
